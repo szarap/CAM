24730525195fc6800526bc535cf082fa9ca03455
szarap <lic.szgonzalez@gmail.com>
2021-11-13 15:00:19 +0100

    is not part of the project

--- clases/clase_2_pabloGonzalez.js
+++
@@ -1,9 +0,0 @@
-var respuesta = prompt("de que color es el cielo?\n 1- celeste \n 2- otro color");
-    respuesta = respuesta.toLowerCase();
-if (respuesta == "celeste"|| respuesta == 1) {
-    alert ("El color ingresado es Correcto!");
-}
-else if (respuesta == 2 || respuesta == "otro color") { 
-    alert ("segui participando");
-}
-
